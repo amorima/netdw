d34bea7551785bb42197893bc357643c029f7b62
Antonio Amorim <work@antonioamorim.pt>
2026-03-01 16:25:06 +0000

    Add favicon images and web manifest for NeTDW
    - Added favicon-16x16.png and favicon-32x32.png to enhance site branding.
    - Introduced favicon.ico for broader browser compatibility.
    - Created site.webmanifest to support Progressive Web App features, including app name, icons, and theme colors.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,39 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      meta: [
+        {
+          name: "theme-color",
+          content: "#050812",
+        },
+      ],
       link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
         {
           rel: "stylesheet",
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css",
